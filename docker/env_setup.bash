529d5aa5efaae6807aab9bfc4652420e13a5abb4
kirillin <kaartemov@gmail.com>
2020-12-13 21:21:39 +0300

    Add docker files

---
+++ docker/env_setup.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source /projects/robotology-superbuild/build/install/share/robotology-superbuild/setup.sh
+source /opt/ros/melodic/setup.bash
+#echo "192.168.1.182 10000 yarp" > /root/.config/yarp/yarp.conf
